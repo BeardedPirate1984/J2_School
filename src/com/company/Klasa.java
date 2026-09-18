3cf941d9083235f0719ba3baebdb3c36bf19b1f3
mechaniktgmobilne <marzkup@gmail.com>
2026-09-18 09:29:15 +0200

    klasa Klasa jako zespol

---
+++ src/com/company/Klasa.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.SortedMap;
+
+public class Klasa {
+    private ArrayList<Uczen> uczniowie;
+    private Wychowawca wychowawca;
+    private String nazwa;
+    private int poziom;
+
+    public Klasa(Wychowawca wychowawca, int poziom, String kierunek) {
+        this.wychowawca = wychowawca;
+        this.poziom = poziom;
+        nazwa = kierunek + poziom;
+        uczniowie = new ArrayList<Uczen>();
+    }
+
+    public boolean dodajUczniaDoKlasy (Uczen uczen){
+        if(uczniowie.contains(uczen)){
+            System.out.println(uczen+ " jest już w tej klasie");
+            return false;
+        }
+        uczniowie.add(uczen);
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Klasa{" +
+                "uczniowie=" + uczniowie +
+                ", wychowawca=" + wychowawca +
+                ", nazwa='" + nazwa + '\'' +
+                ", poziom=" + poziom +
+                '}';
+    }
+
+    public void wypiszInfo(){
+        System.out.println("KLasa: "+nazwa);
+        System.out.println("Wychowawca "+wychowawca.imie);
+        for (int i = 0; i < uczniowie.size(); i++) {
+            System.out.println("Uczen "+i+": "+uczniowie.get(i));
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,21 @@ public class Main {
         Osoba uczen = new Uczen("Edek",8);
 
         Uczen uczen1 = new Uczen("Ala",7);
+        Uczen uczen2 = new Uczen("Ela",7);
+        Uczen uczen3 = new Uczen("Ola",7);
         System.out.println(uczen);
         System.out.println(uczen1);
         Wychowawca wychowawca1P = new Wychowawca("Zygmund","wf");
         System.out.println(wychowawca1P);
         uczen1.wykonaDyzur();
         wychowawca1P.wykonaDyzur();
+        Klasa klasa1P = new Klasa(wychowawca1P,1,"P");
+        klasa1P.dodajUczniaDoKlasy(uczen1);
+        klasa1P.dodajUczniaDoKlasy(uczen1);
+        klasa1P.dodajUczniaDoKlasy(uczen2);
+        klasa1P.dodajUczniaDoKlasy(uczen3);
+        System.out.println(klasa1P);
+
+klasa1P.wypiszInfo();
     }
 }
